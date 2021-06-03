35c38827962cdc6320712ef0364bf1e9d58f3592
rezaatae <r.atae@yahoo.com>
2021-06-03 18:52:19 +0100

    added api and redux files. need to fix axios bug

--- server/index.js
+++ server/index.js
@@ -7,12 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = "mongodb+srv://javascriptmastery:javascriptmastery123123@cluster0.y4rhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
